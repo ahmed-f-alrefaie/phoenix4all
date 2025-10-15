13386a676db3b3bd438321e851dbdcafd8f9f11e
Ahmed F. Al-Refaie <ahmed.f.alrefaie@gmail.com>
2025-10-15 12:39:25 +0100

    Removed FTP for now

--- src/phoenix4all/net/ftp.py
+++
@@ -1,9 +0,0 @@
-import ftplib
-
-
-def list_ftp_files(ftp_server: str, usr: str = "", password: str = "") -> list:
-    """List files in a given directory on an FTP server."""
-    with ftplib.FTP(host=ftp_server) as ftp:
-        ftp.login()  # Anonymous login
-        files = ftp.nlst()
-    return files
